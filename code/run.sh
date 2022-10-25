4e02427bdd423b5962ac6c46a031e9967fdf4446
Xiao Wang <wangxiao1254@gmail.com>
2022-10-24 19:53:49 -0500

    Update run.sh

--- code/run.sh
+++ code/run.sh
@@ -5,12 +5,3 @@ do
 	cat data.txt | python3 code/parse.py $i > $i.html
 	sleep 5
 done
-
-for i in `seq 199 202`
-do
-	echo $i
-	cat data.txt | python3 code/parse.py $i > "${i}0s".html
-	sleep 5
-done
-
-cat data.txt | python3 code/parse.py -1 > all.html

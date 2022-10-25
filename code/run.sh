25d4b327dbc3dd136468ce55c3b147b950f62bd0
Xiao Wang <wangxiao1254@gmail.com>
2022-10-24 20:01:08 -0500

    Update run.sh

--- code/run.sh
+++ code/run.sh
@@ -1,4 +1,3 @@
-rm *.html
 curyear=`date +'%Y'`
 for i in `seq 1996 "$curyear"`
 do
@@ -6,3 +5,12 @@ do
 	python3 code/parse.py $i data.txt > $i.html
 	sleep 5
 done
+
+for i in `seq 199 202`
+do
+	echo $i
+	python3 code/parse.py $i data.txt > "${i}0s".html
+	sleep 5
+done
+
+python3 code/parse.py -1 data.txt > all.html

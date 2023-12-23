6c9ee2089c0d1e0f39b030a0e68b090be2c0e22d
Xiao Wang <wangxiao1254@gmail.com>
2023-12-22 22:04:49 -0600

    update

--- code/index.py
+++ code/index.py
@@ -1,8 +1,8 @@
 import sys
 
 print(
-"<html>\n
-    <head>\n
-        <meta http-equiv='refresh' content='0; URL=http://eprint-classic.github.io/"+sys.argv[1]+".html'>\n
-    </head>\n
+"<html>\n\
+    <head>\n\
+        <meta http-equiv='refresh' content='0; URL=http://eprint-classic.github.io/"+sys.argv[1]+".html'>\n\
+    </head>\n\
  </html>\n")

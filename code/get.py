b5445398a345f7eb335ad0f121692e30837fa872
Xiao Wang <wangxiao1254@gmail.com>
2024-01-01 18:42:02 -0600

    update

--- code/get.py
+++ code/get.py
@@ -1,26 +1,21 @@
-from oaipmh.client import Client
-from oaipmh.metadata import MetadataRegistry, oai_dc_reader
+from sickle import Sickle
 
-
-URL = 'https://eprint.iacr.org/oai'
-registry = MetadataRegistry()
-registry.registerReader('oai_dc', oai_dc_reader)
-client = Client(URL, registry)
 papers = []
 
-for record in client.listRecords(metadataPrefix='oai_dc'):
-    if record[1] is not None:
-        data = record[1].getMap()
+s = Sickle('https://eprint.iacr.org/oai')
+rs = s.ListRecords(metadataPrefix='oai_dc', ignore_deleted=True)
+for record in rs:
+    data = record.metadata
 
-        p_year = data["identifier"][0][len("https://eprint.iacr.org/"):].split("/")[0]
-        id = data["identifier"][0][len("https://eprint.iacr.org/"):].split("/")[1]
-        title = data["title"][0]
-        authors = data["creator"]
-        for i in range(len(authors)):
-            if authors[i].startswith("and "):
-                authors[i] = authors[i][3:]
+    p_year = data["identifier"][0][len("https://eprint.iacr.org/"):].split("/")[0]
+    id = data["identifier"][0][len("https://eprint.iacr.org/"):].split("/")[1]
+    title = data["title"][0]
+    authors = data["creator"]
+    for i in range(len(authors)):
+        if authors[i].startswith("and "):
+            authors[i] = authors[i][3:]
 
-        papers.append((p_year, id, title, ", ".join(authors)))
+    papers.append((p_year, id, title, ", ".join(authors)))
 
 papers.sort(key=lambda x: 10000*int(x[0])+int(x[1]), reverse=True)
 

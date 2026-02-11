64cbdad0904374e7a16defa2ac4c67be560c2b35
Xiao Wang <wangxiao1254@gmail.com>
2026-02-11 09:02:11 -0600

    Refactor OAI record retrieval using Scythe

--- code/get.py
+++ code/get.py
@@ -1,21 +1,30 @@
-from sickle import Sickle
+from oaipmh_scythe import Scythe
+
+BASE_URL = "https://eprint.iacr.org/oai"
+
+NS = {
+    "oai_dc": "http://www.openarchives.org/OAI/2.0/oai_dc/",
+    "dc": "http://purl.org/dc/elements/1.1/"
+}
 
 papers = []
 
-s = Sickle('https://eprint.iacr.org/oai')
-rs = s.ListRecords(metadataPrefix='oai_dc', ignore_deleted=True)
-for record in rs:
-    data = record.metadata
 
-    p_year = data["identifier"][0][len("https://eprint.iacr.org/"):].split("/")[0]
-    id = data["identifier"][0][len("https://eprint.iacr.org/"):].split("/")[1]
-    title = data["title"][0]
-    authors = data["creator"]
-    for i in range(len(authors)):
-        if authors[i].startswith("and "):
-            authors[i] = authors[i][3:]
+with Scythe(BASE_URL) as scythe:
+    records = scythe.list_records(
+    )
 
-    papers.append((p_year, id, title, ", ".join(authors)))
+    for record in records:
+        if record.deleted:
+            continue
+
+        title = record.xml.find(".//dc:title", namespaces=NS)
+        iden = record.xml.find(".//dc:identifier", namespaces=NS).text
+        id = iden[len("https://eprint.iacr.org/"):].split("/")[1]
+        p_year = iden[len("https://eprint.iacr.org/"):].split("/")[0]
+        authors = record.xml.findall(".//dc:creator", namespaces=NS)
+        authors2 = [i.text for i in authors]
+        papers.append((p_year, id, title, ", ".join(authors2)))
 
 papers.sort(key=lambda x: 10000*int(x[0])+int(x[1]), reverse=True)
 
